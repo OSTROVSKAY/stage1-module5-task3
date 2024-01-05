903f1dac202d235363c5b7ae7c1b219b603ec226
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2024-01-05 11:56:52 +0300

    Finish : Stream API task

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,63 @@
 package com.epam.mjc;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        List<Integer> list1 = new LinkedList<>();
+
+        list.stream().filter(i -> i % 2 == 0).forEach(i -> list1.add(i));
+
+        return list1;
+
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        List<String> list1 = new LinkedList<>();
+
+        list.stream().map(i -> i.toUpperCase()).forEach(i -> list1.add(i));
+
+        return list1;
+
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        Integer max = list.stream().mapToInt(i -> i).max().orElseThrow();
+
+        Optional<Integer> expected = Optional.of(max);
+
+        return expected;
+
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        List<Integer> flatList = new LinkedList<>();
+
+        flatList = list.stream().flatMap(List::stream).collect(Collectors.toList());
+
+        Integer min = flatList.stream().mapToInt(i -> i).min().orElseThrow();
+
+        Optional<Integer> expected = Optional.of(min);
+
+        return expected;
+
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        Optional result1 = list.stream().reduce((x,y) -> x + y);
+
+        Integer result = (Integer) result1.get();
+
+        return result;
+
     }
 }
